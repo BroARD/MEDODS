47ab925bfa29ce9622e044a2bb50e7e768aa61db
Вадим Бард <yvadis@mail.ru>
2025-07-05 12:23:30 +0700

    Добавил WebHook

--- internal/auth/usecase/webhook.go
+++ internal/auth/usecase/webhook.go
@@ -1,6 +1,37 @@
 package usecase
 
+import (
+	"Medods/pkg/logging"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
 type WebHookPayload struct {
 	UserID string
-	IP string
+	IP     string
+	Event  string
+}
+
+func sendWebHook(url string, whpayload WebHookPayload, logger logging.Logger) {
+	jsonData, err := json.Marshal(whpayload)
+	if err != nil {
+		logger.Info("WebHook: ошибка преобразования payload в json")
+		return
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		logger.Info("Webhook: ошибка формирования request")
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: time.Second*5}
+	resp, err := client.Do(req)
+	if err != nil {
+		logger.Info("Webhook: ошибка связи с клиентом")
+	}
+
+	defer resp.Body.Close()
 }
